a75d0a1e1fbd88312c433b31d799c3c5f48233cc
anhhoang <anhhoang0319@gmail.com>
2024-03-23 16:07:41 -0700

    ???

--- src/Labs/a3/HelloWorld.tsx
+++ src/Labs/a3/HelloWorld.tsx
@@ -1,14 +1,12 @@
-import {Link} from "react-router-dom";
-import Nav from "../../Nav";
-
+import { Link } from "react-router-dom";
+import Nav from "../../../../../kanbas-quiz-project/kanbas-quiz-project/src/Nav";
 
 function HelloWorld() {
   return (
     <div>
-      <Nav/>
+      <Nav />
       <h1>Hello World!</h1>
     </div>
-
   );
 }
 
